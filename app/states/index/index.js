c4c6b3ed2c8accd956c7860e740644531c41ca01
Andrew Finegan <andrew.finegan@gmail.com>
2017-05-11 03:22:45 -0400

    click markers google maps

--- app/states/index/index.js
+++ app/states/index/index.js
@@ -1,9 +1,37 @@
 (function (angular) {
 	'use strict';
 
-	function dynamicDCSUIController ($scope) {
-		$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+	function dynamicDCSUIController ($scope, uiGmapGoogleMapApi) {
+
+
+		//$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+		$scope.map = {
+			center: {
+				latitude: 42.3349940452867,
+				longitude: -71.0353168884369
+			},
+			zoom: 11,
+			markers: [],
+			events: {
+				click: function (map, eventName, originalEventArgs) {
+					var e = originalEventArgs[0];
+					var lat = e.latLng.lat(),
+						lon = e.latLng.lng();
+					var marker = {
+						id: Date.now(),
+						coords: {
+							latitude: lat,
+							longitude: lon
+						}
+					};
+					$scope.map.markers.push(marker);
+					console.log($scope.map.markers);
+					$scope.$apply();
+				}
+			}
+		};
 	}
+
 	dynamicDCSUIController.$inject = ['$scope'];
 
 	function configFunction($stateProvider) {
