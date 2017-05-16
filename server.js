69a85fdbb99b937067120c87575f089ed7d0b283
Andrew Finegan <andrew.finegan@gmail.com>
2017-05-15 20:44:52 -0400

    bootstrap theme is in place!

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 // app.use/routes/etc...
 app.use('/', express.static(__dirname + '/dist'));
 app.use('/json', express.static(__dirname + '/app/assets/json'));
+app.use('/css', express.static(__dirname + '/app/assets/css'));
 app.use('/tabs', express.static(__dirname + '/app/tabs'));
 app.use('/libs', express.static(__dirname + '/node_modules'));
 
