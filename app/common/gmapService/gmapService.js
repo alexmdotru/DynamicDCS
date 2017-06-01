cf7ef2b442affaf7dfd0971f32259acb64cc9ab9
Andrew Finegan <andrew.finegan@gmail.com>
2017-05-31 22:42:34 -0400

    removing slice

--- app/common/gmapService/gmapService.js
+++ app/common/gmapService/gmapService.js
@@ -218,7 +218,7 @@
 				_.set(sidOpt, 'speed', Math.round(unit.speed) + ' kt');
 			}
 			if (unit.playername !== '') {
-				_.set(sidOpt, 'quantity', unit.playername.slice(0,9));
+				_.set(sidOpt, 'type', unit.playername);
 			}
 
 
