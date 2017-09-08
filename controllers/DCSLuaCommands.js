f68ccf55085f7d486ada1dd2ed2aff5fad327da4
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-08 01:45:37 -0400

    weapon fixes

--- controllers/DCSLuaCommands.js
+++ controllers/DCSLuaCommands.js
@@ -8,12 +8,14 @@ _.set(exports, 'sendMesgToAll', function (serverName, mesg, time) {
 	var curCMD = 'trigger.action.outText("'+mesg+'", '+time+')';
 	var sendClient = {action: "CMD", cmd: curCMD, reqID: 0};
 	var actionObj = {actionObj: sendClient, queName: 'clientArray'};
-	dbMapServiceController.cmdQueActions('save', serverName, actionObj);
+	console.log('STA: ', serverName, actionObj);
+	// dbMapServiceController.cmdQueActions('save', serverName, actionObj);
 });
 
 _.set(exports, 'sendMesgToCoalition', function (coalition, serverName, mesg, time) {
 	var curCMD = 'trigger.action.outTextForCoalition('+coalition+', "'+mesg+'", '+time+')';
 	var sendClient = {action: "CMD", cmd: curCMD, reqID: 0};
 	var actionObj = {actionObj: sendClient, queName: 'clientArray'};
-	dbMapServiceController.cmdQueActions('save', serverName, actionObj);
+	console.log('SMTC: ', serverName, actionObj);
+	// dbMapServiceController.cmdQueActions('save', serverName, actionObj);
 });
