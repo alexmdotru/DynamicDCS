3c42c44ee9fccfbfc381523700fe2ec6bfcfd87f
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-15 05:45:42 -0400

    refixing socket

--- app/common/eventService/event.js
+++ app/common/eventService/event.js
@@ -8,7 +8,6 @@
 		_.set(eCtrl, 'curScore', {});
 
 		_.set(eCtrl, 'byUcid', function (newEvents) {
-			console.log('NE', newEvents);
 			var curiPlayer;
 			var curtPlayer;
 			var scoreMath;
@@ -24,16 +23,22 @@
 					if (curiPlayer) {
 						_.set(eCtrl, ['events', curiPlayer, 'data'], _.get(eCtrl, ['events', curiPlayer, 'data'], []));
 						if (!_.get(eCtrl, ['events', curiPlayer, 'name'])) {
+							_.set(eCtrl, ['curScore', curiPlayer, 'name'], _.get(event, 'iName'))
+						}
+						if (!_.get(eCtrl, ['events', curiPlayer, 'name'])) {
 							_.set(eCtrl, ['events', curiPlayer, 'name'], _.get(event, 'iName'));
 							_.set(eCtrl, ['events', curiPlayer, 'id'], _.get(event, 'iName'));
 						}
-						scoreMath = _.get(eCtrl, ['curScore', curiPlayer], 0) + _.get(event, 'score', 0);
+						scoreMath = _.get(eCtrl, ['curScore', curiPlayer, 'score'], 0) + _.get(event, 'score', 0);
 						if (scoreMath < 0) {
 							scoreMath = 0;
 						}
-						_.set(eCtrl, ['curScore', curiPlayer], scoreMath);
+						_.set(eCtrl, ['curScore', curiPlayer, 'score'], scoreMath);
 					} else {
 						_.set(eCtrl, ['events', curtPlayer, 'data'], _.get(eCtrl, ['events', curtPlayer, 'data'], []));
+						if (!_.get(eCtrl, ['events', curtPlayer, 'name'])) {
+							_.set(eCtrl, ['curScore', curtPlayer, 'name'], _.get(event, 'tName'))
+						}
 						if (!_.get(eCtrl, ['events', curtPlayer], 'name')) {
 							_.set(eCtrl, ['events', curtPlayer, 'name'], _.get(event, 'tName'));
 							_.set(eCtrl, ['events', curtPlayer, 'id'], _.get(event, 'tName'));
@@ -43,7 +48,7 @@
 						enabled: true,
 						radius: 3
 					});
-					_.set(event, 'y',_.get(eCtrl, ['curScore', curiPlayer], 0));
+					_.set(event, 'y',_.get(eCtrl, ['curScore', curiPlayer, 'score'], 0));
 					_.set(event, 'x', new Date(_.get(event, 'createdAt')).getTime());
 					if (curiPlayer) {
 						eCtrl.events[curiPlayer].data.push(event);
