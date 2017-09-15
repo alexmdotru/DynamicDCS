e661bb600cab38fb03c9077e621223211a7c013a
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-15 05:35:48 -0400

    refixing socket

--- app/common/eventService/event.js
+++ app/common/eventService/event.js
@@ -5,16 +5,20 @@
 		var eCtrl = this;
 		var ePromise;
 		_.set(eCtrl, 'events', {});
+		_.set(eCtrl, 'curScore', {});
 
 		_.set(eCtrl, 'byUcid', function (newEvents) {
 			console.log('NE', newEvents);
 			var curiPlayer;
 			var curtPlayer;
 			var scoreMath;
-			var curScore = {};
 			var name;
 			var sortedEvents = _.sortBy(newEvents, ['createdAt']);
 			_.forEach(sortedEvents, function (event) {
+				if (_.get(event, 'createdAt') || _.get(event, 'x')) {
+					_.set(event, 'createdAt', new Date().toISOString());
+				}
+
 				curiPlayer = _.get(event, 'iucid');
 				curtPlayer = _.get(event, 'tucid');
 				if (curiPlayer || curtPlayer) {
@@ -24,11 +28,11 @@
 							_.set(eCtrl, ['events', curiPlayer, 'name'], _.get(event, 'iName'));
 							_.set(eCtrl, ['events', curiPlayer, 'id'], _.get(event, 'iName'));
 						}
-						scoreMath = _.get(curScore, [curiPlayer], 0) + _.get(event, 'score', 0);
+						scoreMath = _.get(eCtrl, ['curScore', curiPlayer], 0) + _.get(event, 'score', 0);
 						if (scoreMath < 0) {
 							scoreMath = 0;
 						}
-						_.set(curScore, [curiPlayer], scoreMath);
+						_.set(eCtrl, ['curScore', curiPlayer], scoreMath);
 					} else {
 						_.set(eCtrl, ['events', curtPlayer, 'data'], _.get(eCtrl, ['events', curtPlayer, 'data'], []));
 						if (!_.get(eCtrl, ['events', curtPlayer], 'name')) {
@@ -40,7 +44,7 @@
 						enabled: true,
 						radius: 3
 					});
-					_.set(event, 'y',_.get(curScore, [curiPlayer], 0));
+					_.set(event, 'y',_.get(eCtrl, ['curScore', curiPlayer], 0));
 					_.set(event, 'x', new Date(_.get(event, 'createdAt')).getTime());
 					if (curiPlayer) {
 						eCtrl.events[curiPlayer].data.push(event);
