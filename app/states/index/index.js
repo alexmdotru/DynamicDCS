32dcbfa53af3aba1554c14c0e76c96f68b1c70ef
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-19 03:45:57 -0400

    refixing socket

--- app/states/index/index.js
+++ app/states/index/index.js
@@ -29,6 +29,11 @@
 			exporting: {
 				enabled: true
 			},
+			tooltip: {
+				pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+				valueDecimals: 2,
+				split: true
+			},
 			legend: {
 				enabled: true,
 				layout: 'horizontal',
