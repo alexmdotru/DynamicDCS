a4c63545c46358634b2e833056ec3c2d4480704f
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-19 20:59:04 -0400

    refixing socket

--- app/common/eventService/event.js
+++ app/common/eventService/event.js
@@ -76,8 +76,8 @@
 				_.set(simpleFlags, 'x', eventTime);
 				_.set(simpleFlags, 'title', _.get(event, 'eventCode'));
 				_.set(simpleFlags, 'text', _.get(event, 'msg'));
-				console.log('SA: ', simpleArray, simpleFlags);
 				eCtrl.events[curPlayer].data.push(simpleArray);
+				eCtrl.events[curPlayer+'F'].data.push(simpleArray);
 			});
 			_.set(eCtrl, 'topScore', _.sortBy(_.values(_.get(eCtrl, 'curScore')), 'score').reverse());
 		});
