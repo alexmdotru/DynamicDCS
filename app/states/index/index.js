8e1a6d33add854c0102db7c9112fc6a2f2c92665
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-20 02:14:13 -0400

    refixing socket

--- app/states/index/index.js
+++ app/states/index/index.js
@@ -1,11 +1,11 @@
 (function (angular) {
 	'use strict';
 
-	function indexController (chartConfig, mySocket, eventService) {
+	function indexController (mySocket, eventService) {
 		var indxCtrl = this;
 		_.set(indxCtrl, 'eventService', eventService);
 
-		console.log('chart: ', chartConfig);
+		console.log('chart: ', this.chartConfig);
 
 		mySocket.emit('room', {
 			server: 'DynamicCaucasus_leaderboard'
@@ -112,7 +112,7 @@
 			series: eventService.events
 		});
 	}
-	indexController.$inject = ['chartConfig', 'mySocket', 'eventService'];
+	indexController.$inject = ['mySocket', 'eventService'];
 
 	function configFunction($stateProvider) {
 		$stateProvider
