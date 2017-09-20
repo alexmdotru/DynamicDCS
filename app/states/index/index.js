2bc68613a6c4d32a2d9d4fface66be1a59197638
Andrew Finegan <andrew.finegan@gmail.com>
2017-09-20 02:48:21 -0400

    refixing socket

--- app/states/index/index.js
+++ app/states/index/index.js
@@ -5,7 +5,7 @@
 		var indxCtrl = this;
 		_.set(indxCtrl, 'eventService', eventService);
 
-		console.log('chart: ', $scope.chartConfig());
+		console.log('chart: ', $scope.chartConfig);
 
 		mySocket.emit('room', {
 			server: 'DynamicCaucasus_leaderboard'
