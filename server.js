6e9f112a7b34d7f3ba285d06ce986fa324959051
Andrew Finegan <andrew.finegan@gmail.com>
2017-10-01 07:37:45 -0400

    cleaning up graph since it cant handle immense data

--- server.js
+++ server.js
@@ -702,22 +702,18 @@ _.set(curServers, 'processQue', function (serverName, sessionName, update) {
 										.then(function (resp) {
 											var switchedPlayerSocket = nonaccountUsers[player.ucid];
 											if(switchedPlayerSocket) {
-												console.log('SWPL: ', switchedPlayer, resp);
-												switchedPlayer = _.find(resp, {ucid: player.ucid});
-												if(switchedPlayer) {
-													if (switchedPlayer.permLvl < 20) {
-														setSocketRoom(switchedPlayerSocket, serverName + '_padmin');
-													} else if (player.side === 1 || player.side === 2) {
-														setSocketRoom(switchedPlayerSocket, serverName + '_q' + player.side);
-														sendInit(serverName, switchedPlayerSocket);
-													}
-												} else {
-													if (player.side === 1 || player.side === 2) {
-														setSocketRoom(switchedPlayerSocket, serverName + '_q' + player.side);
-														sendInit(serverName, switchedPlayerSocket);
-													}
+												if (player.side === 1 || player.side === 2) {
+													setSocketRoom(switchedPlayerSocket, serverName + '_q' + player.side);
+													sendInit(serverName, switchedPlayerSocket);
 												}
-
+											} else {
+												switchedPlayer = _.find(resp, {ucid: player.ucid});
+												if (switchedPlayer.permLvl < 20) {
+													setSocketRoom(switchedPlayerSocket, serverName + '_padmin');
+												} else if (player.side === 1 || player.side === 2) {
+													setSocketRoom(switchedPlayerSocket, serverName + '_q' + player.side);
+													sendInit(serverName, switchedPlayerSocket);
+												}
 											}
 										})
 										.catch(function (err) {
