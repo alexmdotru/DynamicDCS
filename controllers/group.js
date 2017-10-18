14b4d443d1686f5f88fe3440aafc57dce03bf7fb
Andrew Finegan <andrew.finegan@gmail.com>
2017-10-17 22:23:56 -0400

    country obj setup

--- controllers/group.js
+++ controllers/group.js
@@ -3,42 +3,140 @@ const	_ = require('lodash'),
 
 const dbMapServiceController = require('./dbMapService');
 
-_.set(exports, 'spawnGrndUnit', function (serverName, groupObj, routeArry, unitArry) {
-	var curGroup = {};
-	var cRnd = _.random(1000000,9000000);
+// from my main mission object, can spawn units on both sides in this setup
+var countryObj = {
+	side: {
+		0: 'neutral',
+		1: 'red',
+		2: 'blue'
+	},
+	red: [
+		'ABKHAZIA',
+		'BELARUS',
+		'CHINA',
+		'EGYPT',
+		'FINLAND',
+		'HUNGARY',
+		'INSURGENTS',
+		'IRAN',
+		'FRANCE',
+		'ISRAEL',
+		'JAPAN',
+		'KAZAKHSTAN',
+		'NORTH_KOREA',
+		'PAKISTAN',
+		'ROMANIA',
+		'RUSSIA',
+		'SAUDI_ARABIA',
+		'SERBIA',
+		'SLOVAKIA',
+		'SOUTH_OSETIA',
+		'SYRIA',
+		'UKRAINE'
+	],
+	blue: [
+		'AUSTRALIA',
+		'AUSTRIA',
+		'BELGIUM',
+		'BULGARIA',
+		'CANADA',
+		'CROATIA',
+		'CHEZH_REPUBLI',
+		'DENMARK',
+		'IRAQ',
+		'GEORGIA',
+		'GERMANY',
+		'GREECE',
+		'INDIA',
+		'ITALY',
+		'NORWAY',
+		'POLAND',
+		'SOUTH_KOREA',
+		'SPAIN',
+		'SWEDEN',
+		'SWITZERLAND',
+		'THE_NETHERLANDS',
+		'SWITZERLAND',
+		'UK',
+		'USA',
+		'AGGRESSORS',
+	]
+};
 
-	var newSpawnCountry = 'USA';
-	var newSpawnCategory = 'GROUND';
-	var newVehicleType = 'AAV7';
-
-	var defGndGroup = '{' +
-		'["groupId"] = ' + _.get(groupObj, 'groupId', cRnd) + ',' +
-		'["name"] = "' + _.get(groupObj, 'groupName', _.get(groupObj, 'baseName') + '_' + cRnd) + '",' +
+_.set(exports, 'grndUnitGroup', function ( groupObj ) {
+	return '{' +
+		'["groupId"] = ' + _.get(groupObj, 'groupId') + ',' +
+		'["name"] = "' + _.get(groupObj, 'groupName') + '",' +
 		'["visible"] = false,' +
 		'["hidden"] = false,' +
 		'["task"] = {},' +
 		'["units"] = {#UNITS},' +
-		'["category"] = Group.Category.' + _.get(groupObj, 'category', newSpawnCategory) +',' +
-		'["country"] = "' + _.get(groupObj, 'country', newSpawnCountry) + '"' +
-		'}';
-
-	var curUnits = '';
-	_.forEach(unitArry, function (unit, k) {
-		var curN = k + 1;
-		var curUnit = '{' +
-			'["x"] = ' + _.get(unit, 'x') + ',' +
-			'["y"] = ' + _.get(unit, 'y') + ',' +
-			'["type"] = "' + _.get(unit, 'type', newVehicleType) +'",' +
-			'["name"] = "' + _.get(unit, 'name', _.get(unit, 'baseName') + '_' + cRnd + '#' + curN ) + '",' +
-			'["unitId"] = ' + _.get(unit, 'unitId', cRnd+curN) + ',' +
-			'["heading"] = 0,' +
-			'["playerCanDrive"] = true,' +
-			'["skill"] = "Excellent"' +
-			'}';
-		curUnits +=  curUnit + ',';
-	});
-
-	curGroup = defGndGroup;
-	curGroup = _.replace(curGroup,"#UNITS",curUnits);
-	return curGroup;
+		'["category"] = Group.Category.' + _.get(groupObj, 'category') + ',' +
+		'["country"] = "' + _.get(groupObj, 'country') + '"' +
+		'}'
+	;
 });
+
+_.set(exports, 'grndUnitTemplate', function ( unitObj ) {
+	return '{' +
+		'["x"] = ' + _.get(unitObj, 'x') + ',' +
+		'["y"] = ' + _.get(unitObj, 'y') + ',' +
+		'["type"] = "' + _.get(unitObj, 'type') +'",' +
+		'["name"] = "' + _.get(unitObj, 'name') + '",' +
+		'["unitId"] = ' + _.get(unitObj, 'unitId') + ',' +
+		'["heading"] = 0,' +
+		'["playerCanDrive"] = true,' +
+		'["skill"] = "Excellent"' +
+		'}'
+	;
+});
+
+_.set(exports, 'spawnSupportBaseGrp', function ( groupObj ) {
+	/* spawn all support and some low end humvee
+		load config vars for server
+		spawn
+	*/
+});
+
+_.set(exports, 'spawnBaseReinforcementGroup', function ( groupObj ) {
+
+});
+
+_.set(exports, 'depopGroup', function ( groupObj ) {
+
+});
+
+_.set(exports, 'repopGroup', function ( groupObj ) {
+
+});
+
+/*
+ spawn random allocations from these groups
+ unarmedAmmo
+ unarmedFuel
+ unarmedPower
+
+ Tank
+ APC
+ armoredCar
+ troop
+ armedStructure
+
+ antiAir
+ samRadar (check if single or group, spawn group is group exist, launcher)
+ samIR
+
+ carrierShip
+ defenseShip
+
+ var newSpawnCountry = 'USA';
+ var newSpawnCategory = 'GROUND';
+ var newVehicleType = 'AAV7';
+
+ var curGroup = {};
+ var cRnd = _.random(1000000,9000000);
+
+ curGroup = defGndGroup;
+ curGroup = _.replace(curGroup,"#UNITS",curUnits);
+ return curGroup;
+ */
