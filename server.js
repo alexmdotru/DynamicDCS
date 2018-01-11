4e82277d0bdbdccd07d275735fc5c0d1b4c32df4
Andrew Finegan <andrew.finegan@gmail.com>
2018-01-10 19:18:41 -0500

    dcs bug weapons give 10 points

--- server.js
+++ server.js
@@ -1189,7 +1189,10 @@ _.set(curServers, 'processQue', function (serverName, sessionName, update) {
 			_.forEach(shootingUsers, function (user, key) {
 				if(_.get(user, ['startTime']) + 1500 < new Date().getTime()){
 					var shootObj = _.get(user, ['iCurObj']);
-					_.set(shootObj, 'score', _.get(shootingUsers, [iUnitId, 'count']));
+					_.set(shootObj, 'score', _.get(shootingUsers, [iUnitId, 'count'], 1));
+					if (shootObj.score === 1) {
+						_.set(shootObj, 'score', 10);
+					}
 					if(_.get(shootObj, 'iucid') || _.get(shootObj, 'tucid')) {
 						curServers[serverName].updateQue.leaderboard.push(_.cloneDeep(iCurObj));
 						dbMapServiceController.simpleStatEventActions('save', _.get(user, ['serverName']), shootObj);
@@ -1230,8 +1233,6 @@ _.set(curServers, 'processQue', function (serverName, sessionName, update) {
 							var curTUnit = _.get(tunit, 0);
 							var iOwnerId = _.get(curIUnit, 'playerOwnerId');
 							var tOwnerId = _.get(curTUnit, 'playerOwnerId');
-
-							console.log('unitInfo: ', {iUnit: iunit, tUnit: tunit});
 
 							if (iOwnerId || tOwnerId) {
 								if (iOwnerId) {
@@ -1327,17 +1328,21 @@ _.set(curServers, 'processQue', function (serverName, sessionName, update) {
 										;
 									} else {
 										var shotCount;
+										var shotpoints;
 										// console.log('weapon not here');
-										// console.log('weapon: ', _.get(queObj, ['data', 'arg7', 'typeName']));
+										// console.log('Weapon Unknown: ', _.get(queObj, ['data', 'arg7', 'typeName']));
 										_.set(shootingUsers, [iUnitId, 'count'], _.get(shootingUsers, [_.get(iCurObj, 'iPlayerUnitId'), 'count'], 0)+1);
 										_.set(shootingUsers, [iUnitId, 'startTime'], new Date().getTime());
 										_.set(shootingUsers, [iUnitId, 'serverName'], serverName);
-										shotCount = _.get(shootingUsers, [iUnitId, 'count'], 0);
-										_.set(iCurObj, 'score', 1);
+										shotCount = _.get(shootingUsers, [iUnitId, 'count'], 1);
+										if (shotCount === 1) {
+											shotpoints = 10;
+										} else {
+											shotpoints = shotCount
+										}
 										_.set(iCurObj, 'msg',
-											'A: '+ getSide(_.get(curIUnit, 'coalition'))+' '+ iPName +' has hit '+getSide(_.get(curTUnit, 'coalition'))+' ' + tPName + ' '+shotCount+' times with machine gun - +' + shotCount
+											'A: '+ getSide(_.get(curIUnit, 'coalition'))+' '+ iPName +' has hit '+getSide(_.get(curTUnit, 'coalition'))+' ' + tPName + ' '+shotCount+' times with ?(dcsbug) - +' + shotpoints
 										);
-										dbMapServiceController.simpleStatEventActions('save', serverName, iCurObj);
 										_.set(shootingUsers, [iUnitId, 'iCurObj'], _.cloneDeep(iCurObj));
 									}
 								})
@@ -1370,7 +1375,7 @@ _.set(curServers, 'processQue', function (serverName, sessionName, update) {
 
 			dbMapServiceController.unitActions('read', serverName, {_id: _.get(queObj, ['data', 'arg3'])})
 				.then(function (iunit) {
-					var curIUnit = _.get(iunit, 0);
+				var curIUnit = _.get(iunit, 0);
 					if (curIUnit) {
 						iPlayer = _.find(curServers[serverName].serverObject.players, {name: _.get(curIUnit, 'playername')});
 						if (iPlayer) {
