bb9657cc5aae803e01753e02e8d9c4d5064cdf52
Andrew Finegan <andrew.finegan@gmail.com>
2018-01-27 02:05:04 -0500

    code cleanup

--- controllers/events/backend/disconnect.js
+++ controllers/events/backend/disconnect.js
@@ -3,13 +3,12 @@ const constants = require('../../constants');
 const dbMapServiceController = require('../../db/dbMapService');
 const DCSLuaCommands = require('../../player/DCSLuaCommands');
 const playersEvent = require('../../events/backend/players');
-const capLivesController = require('../../action/capLives');
 
 _.set(exports, 'processDisconnect', function (serverName, sessionName, eventObj) {
 	var iPlayer;
 	var iCurObj;
 	// "disconnect", playerID, name, playerSide, reason_code
-	iPlayer = _.find(_.get(playersEvent, ['rtPlayerArray', serverName]), {id: eventObj.data.arg1});
+	iPlayer = _.find(playersEvent.rtPlayerArray[serverName], {id: eventObj.data.arg1});
 	if (iPlayer) {
 		iCurObj = {
 			sessionName: sessionName,
@@ -28,24 +27,6 @@ _.set(exports, 'processDisconnect', function (serverName, sessionName, eventObj)
 				})
 			;
 		}
-
-		console.log('1 ');
-		dbMapServiceController.unitActions('read', serverName, {playername: iPlayer.name})
-			.then(function (iunit) {
-				var curUnit = _.get(iunit, [0]);
-				console.log('2 ', curUnit);
-				console.log('Disconnected: ', _.includes(capLivesController.capLivesEnabled, curUnit.type), curUnit.inAir);
-				if (_.includes(capLivesController.capLivesEnabled, curUnit.type) && curUnit.inAir) {
-					//take life away
-					console.log('3 ', curUnit);
-					capLivesController.removeLife(serverName, iPlayer.ucid, curUnit.groupId);
-				}
-			})
-			.catch(function (err) {
-				console.log('err line45: ', err);
-			})
-		;
-
 		DCSLuaCommands.sendMesgToAll(
 			serverName,
 			_.get(iCurObj, 'msg'),
