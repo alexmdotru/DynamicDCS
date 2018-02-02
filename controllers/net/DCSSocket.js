0eebcf01011bfde8e1436212f42541759845ee52
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-02 13:45:28 -0500

    lengthen the sync speed

--- controllers/net/DCSSocket.js
+++ controllers/net/DCSSocket.js
@@ -4,7 +4,7 @@ const dbMapServiceController = require('../db/dbMapService'); // reqclientArray,
 const sychrontronController = require('../sychronize/Sychrontron');
 
 var lastSyncTime = new Date().getTime();
-var syncSpawnTimer = 10000;
+var syncSpawnTimer = 20000;
 
 exports.createSocket = function (serverName, address, port, queName, callback) {
 	var sock = this;
