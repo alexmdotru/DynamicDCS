fdfabe0754f142dff9166f89a578cb56ca8aa4b5
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-05 22:20:26 -0500

    LAND

--- controllers/sychronize/Sychrontron.js
+++ controllers/sychronize/Sychrontron.js
@@ -17,7 +17,7 @@ _.set(exports, 'syncType', function (serverName, serverUnitCount) {
 	var remappedunits = {};
 	dbMapServiceController.unitActions('readStd', serverName, {dead: false, type: {$ne: 'UAZ-469'}})
 		.then(function (units) {
-			if (serverUnitCount === 0) { //server is empty
+			if (serverUnitCount === 1) { //server is empty
 				exports.isServerSynced = false;
 				isServerFresh = true;
 				if (!exports.isSyncLockdownMode) {
@@ -84,7 +84,7 @@ _.set(exports, 'syncType', function (serverName, serverUnitCount) {
 						DCSLuaCommands.sendMesgChatWindow(serverName, mesg);
 						exports.isServerSynced = false;
 					} else {
-						if (!exports.isServerSynced && units.length > 300) {
+						if (!exports.isServerSynced && units.length > 500) {
 							mesg = 'Server units are Synced, Slots Now Open!';
 							console.log(mesg);
 							DCSLuaCommands.sendMesgChatWindow(serverName, mesg);
@@ -120,7 +120,7 @@ _.set(exports, 'syncType', function (serverName, serverUnitCount) {
 						// DCSLuaCommands.sendMesgChatWindow(serverName, mesg);
 						// exports.isServerSynced = true;
 					} else {
-						if (!exports.isServerSynced && units.length > 300) {
+						if (!exports.isServerSynced && units.length > 500) {
 							mesg = 'Server units Synced';
 							console.log(mesg);
 							//DCSLuaCommands.sendMesgChatWindow(serverName, mesg);
