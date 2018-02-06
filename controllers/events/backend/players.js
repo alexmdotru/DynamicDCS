657151bfa949f6502cb391cc852165acd38d00f3
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-06 04:58:12 -0500

    fix for removing units

--- controllers/events/backend/players.js
+++ controllers/events/backend/players.js
@@ -5,7 +5,7 @@ const DCSLuaCommands = require('../../player/DCSLuaCommands');
 
 _.set(exports, 'processPlayerEvent', function (serverName, sessionName, playerArray) {
 	_.set(exports, ['rtPlayerArray', serverName], playerArray);
-	_.forEach(playerArray, function (player) {
+	_.forEach(playerArray.data, function (player) {
 		if (player !== null) {
 			var curPlyrUcid = player.ucid;
 			var curPlyrSide = player.side;
