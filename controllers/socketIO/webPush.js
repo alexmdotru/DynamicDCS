fa2bf196caf522380e18fa3cc85a44f04a915cbb
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-10 04:03:14 -0500

    getting closer

--- controllers/socketIO/webPush.js
+++ controllers/socketIO/webPush.js
@@ -16,7 +16,6 @@ _.set(exports, 'sendToAll', function (serverName, pData) {
 _.set(exports, 'sendToCoalition', function (serverName, pData) {
 	var coalition = _.get(pData, ['payload', 'data', 'coalition']);
 	var displaySide = _.get(pData, ['payload', 'data', 'displaySide']);
-	var serverName =  _.set(pData, 'serverName', _.toLower(serverName));
 	if(coalition) {
 		_.set(pData, 'side', coalition);
 	} else if (displaySide) {
