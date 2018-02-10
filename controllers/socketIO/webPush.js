83bc729bce890889be29626b5248f2cb3a52adf2
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-09 21:17:50 -0500

    getting closer

--- controllers/socketIO/webPush.js
+++ controllers/socketIO/webPush.js
@@ -4,7 +4,6 @@ const dbMapServiceController = require('../db/dbMapService');
 _.set(exports, 'sendToAll', function (serverName, payload) {
 	for(var x=0; x <= '3'; x++) {
 		_.set(payload, 'side', x);
-		console.log('sta: ', serverName, payload);
 		dbMapServiceController.webPushActions('save', serverName, payload)
 			.catch(function (err) {
 				console.log('line9: ', err);
