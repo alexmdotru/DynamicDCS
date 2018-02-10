6d99d237dec1884f34d1d45a718c5966a57deda2
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-09 21:09:59 -0500

    getting closer

--- controllers/socketIO/webPush.js
+++ controllers/socketIO/webPush.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const dbMapServiceController = require('../../db/dbMapService');
+const dbMapServiceController = require('../db/dbMapService');
 
 _.set(exports, 'sendToAll', function (serverName, payload) {
 	for(x=0; x <= '3', x++;) {
