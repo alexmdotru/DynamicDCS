0b686884d351b9b00f0e62f6290141ae4bd28d19
Andrew Finegan <andrew.finegan@gmail.com>
2018-02-14 01:19:13 -0500

    heavy crates added

--- controllers/action/staticCrates.js
+++ controllers/action/staticCrates.js
@@ -11,7 +11,7 @@ _.set(exports, 'processStaticCrate', function (serverName, crateObj) {
 	var cPromise = [];
 	_.forEach(_.get(crateObj, 'data', {}), function (crate, name) {
 		if(crate.alive) {
-			console.log('ACHK: ', name, );
+			console.log('ACHK: ', name);
 			cPromise.push(dbMapServiceController.staticCrateActions('update', serverName, {_id: name, lonLatLoc: [crate.lon, crate.lat]})
 				.catch(function (err) {
 					console.log('line 17: ', err);
