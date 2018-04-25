0c6a00116faeb6e1b7f15d98c0db02bea3fc60ab
Andrew Finegan <andrew.finegan@gmail.com>
2018-04-25 18:28:15 -0400

    turn long range sames back on

--- servers/dynamicCaucasus/dynamicCaucasusFrontend.js
+++ servers/dynamicCaucasus/dynamicCaucasusFrontend.js
@@ -233,7 +233,7 @@ setInterval(function () {
 setInterval(function () {
 	if (groupController.bases) {
 		if (!_.get(CCB, ['DCSSocket', 'connOpen'], true)) {
-			sychrontronController.syncType(CCB.serverName, _.get(CCB, 'curServerUnitCnt', 40)-40);
+			sychrontronController.syncType(CCB.serverName, _.get(CCB, 'curServerUnitCnt', 47)-47);
 		}
 	} else {
 		groupController.initDbs(CCB.serverName);
