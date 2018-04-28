37e3c0517e9eea32687f60b48381152feac8a4b4
Andrew Finegan <andrew.finegan@gmail.com>
2018-04-28 02:39:39 -0400

    kick for being on wrong side comms update

--- controllers/discordBot/discordBot.js
+++ controllers/discordBot/discordBot.js
@@ -8,23 +8,116 @@ const webPushCommands = require('../socketIO/webPush');
 
 const client = new Discord.Client();
 
+var dBot = {};
 exports.oldestAllowedUser = 300;
 exports.timeToCorrect = 20;
+exports.Only1ChannelNames = [
+	'Red Gen Chat(Relaxed GCI)',
+	'Red GCI Group 1(Brevity)',
+	'Red GCI Group 2(Brevity)'
+];
+exports.Only2ChannelNames = [
+	'Blue Gen Chat(Relaxed GCI)',
+	'Blue GCI Group 1(Brevity)',
+	'Blue GCI Group 2(Brevity)'
+];
+
+_.set(dBot, 'kickForNoComms', function (curServerName, playerArray, discordUserNames) {
+	var pNIC = _.reject(playerArray, function (player) {
+		if (!_.includes(discordUserNames, player.name)) {
+			// console.log('match: ', _.includes(discordUserNames, player.name), player.name, discordUserNames);
+		}
+		return _.includes(discordUserNames, player.name);
+	});
+
+	dbMapServiceController.unitActions('read', curServerName, {playername: {$in: _.compact( _.map(pNIC, 'name'))}})
+		.then(function (pUnits) {
+			console.log('----------------------');
+			_.forEach(pUnits, function (pUnit) {
+				var curPlayer = _.find(playerArray, {name: pUnit.playername});
+				if (curPlayer) {
+					var newLifeCount = (curPlayer.gicTimeLeft === 0)? exports.timeToCorrect : curPlayer.gicTimeLeft - 1 ;
+					if (newLifeCount !== 0) {
+						console.log('GTBK: ', newLifeCount, pUnit.playername);
+						var mesg = "SERVER REQUIREMENT(you have " + newLifeCount + " mins left to fix):You are currently not logged into a discord voice channel or your discord nickname and player name does not match(Case Sensitive!). Please join DDCS discord https://discord.gg/NSzajs7";
+                        DCSLuaCommands.sendMesgToGroup(pUnit.groupId, curServerName, mesg, '60');
+                        dbMapServiceController.srvPlayerActions('update', curServerName, {_id: curPlayer._id, gicTimeLeft: newLifeCount})
+                            .catch(function (err) {
+                                console.log('line58', err);
+                            })
+                        ;
+					} else {
+						dbMapServiceController.srvPlayerActions('update', curServerName, {_id: curPlayer._id, gicTimeLeft: newLifeCount})
+							.then(function () {
+								console.log('KICKED FOR NO COMMS: ', pUnit.playername, pUnit);
+								var mesg = "YOU HAVE BEEN KICKED TO SPECTATOR FOR NOT BEING IN COMMS, You are currently not logged into a discord voice channel or your discord nickname and player name does not match(Case Sensitive!). Please join DDCS discord https://discord.gg/NSzajs7";
+                                DCSLuaCommands.sendMesgToGroup(pUnit.groupId, curServerName, mesg, '60');
+                                DCSLuaCommands.forcePlayerSpectator(curServerName, curPlayer.playerId, mesg);
+							})
+							.catch(function (err) {
+								console.log('line70', err);
+							})
+						;
+					}
+				}
+			})
+		})
+		.catch(function (err) {
+			console.log('line37', err);
+		})
+	;
+});
+
+_.set(dBot, 'kickForOpposingSides', function (serverName, playerArray, discordByChannel) {
+	var moveToChan = client.channels.find("name", 'dynamicdcs.com for map');
+	_.forEach(exports.Only1ChannelNames, function (chanName) {
+		if(discordByChannel[chanName]) {
+			_.forEach(discordByChannel[chanName], function (vcUser, userName) {
+				var findCurPlayer = _.find(playerArray, {name: userName});
+				if(findCurPlayer) {
+					if (findCurPlayer.side !== 1) {
+						console.log('kick user for wrong side GCI: ', userName);
+						vcUser.setVoiceChannel(moveToChan);
+					}
+				}
+			});
+		}
+	});
+	_.forEach(exports.Only2ChannelNames, function (chanName) {
+		if(discordByChannel[chanName]) {
+			_.forEach(discordByChannel[chanName], function (vcUser, userName) {
+				var findCurPlayer = _.find(playerArray, {name: userName});
+				if(findCurPlayer) {
+					if (findCurPlayer.side !== 2) {
+						console.log('kick user for wrong side GCI: ', userName);
+						vcUser.setVoiceChannel(moveToChan);
+					}
+				}
+			});
+		}
+	});
+});
+
+_.set(dBot, 'getName', function (vcUser) {
+	if (vcUser.nickname) {
+		return vcUser.nickname;
+	}
+	return _.get(vcUser, 'user.username');
+});
 
 client.on('ready', () => {
 	console.log('Ready!');
+	dBot.counter = 0;
 	setInterval (function (){
+		var discordByChannel = {};
 		var discordUserNames = ['Drexserver'];
 		var curGuild = client.guilds.get('389682718033707008');
 		var voiceChans = curGuild.channels.filter(ch => ch.type === 'voice');
 		_.forEach(Array.from(voiceChans.values()), function (voiceChan) {
 			_.forEach(Array.from(voiceChan.members.values()), function (vcUser) {
 				// console.log('nick: ', vcUser.nickname, 'un: ', _.get(vcUser, 'user.username'));
-				if (vcUser.nickname) {
-					discordUserNames.push(vcUser.nickname);
-				} else {
-					discordUserNames.push(_.get(vcUser, 'user.username'));
-				}
+				_.set(discordByChannel, [voiceChan.name, dBot.getName(vcUser)], vcUser);
+				discordUserNames.push(dBot.getName(vcUser));
 			});
 		});
 
@@ -37,49 +130,13 @@ client.on('ready', () => {
 							if (latestSession.name) {
 								dbMapServiceController.srvPlayerActions('read', curServerName, {playerId: {$ne: '1'}, name: {$ne: ''}, sessionName: latestSession.name})
 									.then(function (playerArray) {
-										var pNIC = _.reject(playerArray, function (player) {
-											if (!_.includes(discordUserNames, player.name)) {
-												// console.log('match: ', _.includes(discordUserNames, player.name), player.name, discordUserNames);
-											}
-											return _.includes(discordUserNames, player.name);
-										});
-										// console.log('pn: ', _.compact( _.map(pNIC, 'name')));
-										dbMapServiceController.unitActions('read', curServerName, {playername: {$in: _.compact( _.map(pNIC, 'name'))}})
-											.then(function (pUnits) {
-												console.log('----------------------');
-												_.forEach(pUnits, function (pUnit) {
-													var curPlayer = _.find(playerArray, {name: pUnit.playername});
-													if (curPlayer) {
-														var newLifeCount = (curPlayer.gicTimeLeft === 0)? exports.timeToCorrect : curPlayer.gicTimeLeft - 1 ;
-														if (newLifeCount !== 0) {
-															console.log('GTBK: ', newLifeCount, pUnit.playername);
-															var mesg = "SERVER REQUIREMENT(you have " + newLifeCount + " mins left to fix):You are currently not logged into a discord voice channel or your discord nickname and player name does not match(Case Sensitive!). Please join DDCS discord https://discord.gg/NSzajs7";
-															DCSLuaCommands.sendMesgToGroup(pUnit.groupId, curServerName, mesg, '60');
-															dbMapServiceController.srvPlayerActions('update', curServerName, {_id: curPlayer._id, gicTimeLeft: newLifeCount})
-																.catch(function (err) {
-																	console.log('line58', err);
-																})
-															;
-														} else {
-															dbMapServiceController.srvPlayerActions('update', curServerName, {_id: curPlayer._id, gicTimeLeft: newLifeCount})
-																.then(function () {
-																	console.log('KICKED FOR NO COMMS: ', pUnit.playername, pUnit);
-																	var mesg = "YOU HAVE BEEN KICKED TO SPECTATOR FOR NOT BEING IN COMMS, You are currently not logged into a discord voice channel or your discord nickname and player name does not match(Case Sensitive!). Please join DDCS discord https://discord.gg/NSzajs7";
-																	DCSLuaCommands.sendMesgToGroup(pUnit.groupId, curServerName, mesg, '60');
-																	DCSLuaCommands.forcePlayerSpectator(curServerName, curPlayer.playerId, mesg);
-																})
-																.catch(function (err) {
-																	console.log('line70', err);
-																})
-															;
-														}
-													}
-												})
-											})
-											.catch(function (err) {
-												console.log('line37', err);
-											})
-										;
+										if(dBot.counter === 59) {
+											dBot.kickForNoComms(curServerName, playerArray, discordUserNames);
+											dBot.counter = 0;
+										}
+										// have all the existing player names on the server
+										dBot.kickForOpposingSides(curServerName, playerArray, discordByChannel);
+										dBot.counter++;
 									})
 									.catch(function (err) {
 										console.log('line37', err);
@@ -97,7 +154,7 @@ client.on('ready', () => {
 				console.log('line49', err);
 			})
 		;
-	}, 60 * 1000);
+	}, 1 * 1000);
 });
 
 _.set(exports, 'sendSoundBite', function (vcArray, songFile) {
