e8d5cbc1196cfdc13a31bd87865dc48cddc2971b
Andrew Finegan <andrew.finegan@gmail.com>
2018-06-03 00:22:33 -0400

    remove dead update, 5 mins

--- controllers/db/dbMapService.js
+++ controllers/db/dbMapService.js
@@ -21,6 +21,7 @@ var processSchema = require('./models/processSchema');
 
 var oneMin = 60 * 1000;
 var oneHour = 60 * oneMin;
+var removeDead = oneMin * 5;
 
 _.set(exports, 'connectMapDB', function (dynamicHost, dynamicDatabase) {
 	mapdb.open(dynamicHost, dynamicDatabase);
@@ -644,7 +645,16 @@ exports.unitActions = function (action, serverName, obj){
 	}
 	if(action === 'removeAllDead') {
 		return new Promise(function(resolve, reject) {
-			Unit.remove({dead: true, category: {$ne: 'STRUCTURE'}},
+			Unit.remove(
+				{
+					dead: true,
+					category: {
+						$ne: 'STRUCTURE'
+					},
+					date: {
+						$lte: (new Date(new Date()).getTime() - removeDead)
+					}
+				},
 				function(err, units) {
 					if (err) { reject(err) }
 					resolve(units);
