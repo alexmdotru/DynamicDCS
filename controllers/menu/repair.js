cd035e78ba91c6335f2351d37c2f80ded06a257f
Andrew Finegan <andrew.finegan@gmail.com>
2018-06-17 00:58:29 -0400

    remove dead update, 5 mins

--- controllers/menu/repair.js
+++ controllers/menu/repair.js
@@ -5,29 +5,35 @@ const proximityController = require('../proxZone/proximity');
 
 _.set(exports, 'repairBase', function (serverName, curUnit, crateOriginLogiName, crate) {
 	//var baseClose = proximityController.extractUnitsBackToBase(curUnit, serverName);
-	var baseClose = proximityController.getLogiTowersProximity(serverName, curUnit.lonLatLoc, 3);
-	console.log('repairNase: ', baseClose, curUnit, serverName, crateOriginLogiName);
-	if (baseClose + ' Logistics' !== crateOriginLogiName) {
-		groupController.healBase(serverName, baseClose);
-		// baracks
-		// base respawn building
-		if (crate) {
-			groupController.destroyUnit(serverName, crate.name);
-		}
-		DCSLuaCommands.sendMesgToGroup(
-			curUnit.groupId,
-			serverName,
-			"G: " + baseClose + " Base Has Been Repaired!",
-			5
-		);
-		return true;
-	} else {
-		DCSLuaCommands.sendMesgToGroup(
-			curUnit.groupId,
-			serverName,
-			"G: Repair Crate Not Close Enough To Base, or Crate has originated from this base!",
-			5
-		);
-	}
-	return false;
+	proximityController.getLogiTowersProximity(serverName, curUnit.lonLatLoc, 3)
+		.then(function (closeBase) {
+			console.log('repairNase: ', closeBase, curUnit, serverName, crateOriginLogiName);
+			if (closeBase + ' Logistics' !== crateOriginLogiName) {
+				groupController.healBase(serverName, closeBase);
+				// baracks
+				// base respawn building
+				if (crate) {
+					groupController.destroyUnit(serverName, crate.name);
+				}
+				DCSLuaCommands.sendMesgToGroup(
+					curUnit.groupId,
+					serverName,
+					"G: " + closeBase + " Base Has Been Repaired!",
+					5
+				);
+				return true;
+			} else {
+				DCSLuaCommands.sendMesgToGroup(
+					curUnit.groupId,
+					serverName,
+					"G: Repair Crate Not Close Enough To Base, or Crate has originated from this base!",
+					5
+				);
+			}
+			return false;
+		})
+		.catch(function(err) {
+			console.log('err line1072: ', err);
+		})
+	;
 });
