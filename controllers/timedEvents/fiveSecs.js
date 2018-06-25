e2a9ea240b9754b97ab671df0bcfb436c655d5a6
Andrew Finegan <andrew.finegan@gmail.com>
2018-06-25 02:40:27 -0400

    changing to 30 secs

--- controllers/timedEvents/fiveSecs.js
+++ controllers/timedEvents/fiveSecs.js
@@ -2,68 +2,14 @@ const _ = require('lodash');
 const dbMapServiceController = require('../db/dbMapService');
 const groupController = require('../spawn/group');
 const baseSpawnFlagsController = require('../action/baseSpawnFlags');
-const menuUpdateController = require('../menu/menuUpdate');
 
 var replenThreshold = 30; // percentage under max
 var replenTimer = _.random(5400, 9000);
-var AIMaxIdleTime = (5 * 60 * 1000); // 5 mins
-var maxCrateLife = (90 * 60 * 1000); // 90 mins
 
 _.set(exports, 'processFiveSecActions', function (serverName, fullySynced) {
 	if (fullySynced) {
 		//set base flags
 		baseSpawnFlagsController.setbaseSides(serverName);
-
-		//cleanupAI AIMaxIdleTime
-		dbMapServiceController.unitActions('read', serverName, {isAI: true, dead:false})
-			.then(function (AICleanup) {
-				_.forEach(AICleanup, function (AIUnit) {
-					if (_.isEmpty(AIUnit.playername) && new Date(_.get(AIUnit, 'updatedAt', 0)).getTime() + AIMaxIdleTime < new Date().getTime()) {
-						groupController.destroyUnit( serverName, AIUnit.name );
-					}
-				});
-			})
-			.catch(function (err) {
-				console.log('err line20: ', err);
-			})
-		;
-
-		//clean crates older than 90mins
-		if (menuUpdateController.virtualCrates) {
-			dbMapServiceController.unitActions('read', serverName, {isCrate: true, dead:false})
-				.then(function (crateCleanup) {
-					_.forEach(crateCleanup, function (crate) {
-						if (new Date(_.get(crate, 'createdAt', 0)).getTime() + maxCrateLife < new Date().getTime()) {
-							groupController.destroyUnit( serverName, crate.name );
-						}
-					});
-				})
-				.catch(function (err) {
-					console.log('err line42: ', err);
-				})
-			;
-		} else {
-			dbMapServiceController.staticCrateActions('readStd', serverName, {})
-				.then(function (crateCleanup) {
-					_.forEach(crateCleanup, function (crate) {
-						if (new Date(_.get(crate, 'createdAt', 0)).getTime() + maxCrateLife < new Date().getTime()) {
-							dbMapServiceController.staticCrateActions('delete', serverName, {_id: crate._id})
-								.then(function () {
-									console.log('cleanup crate: ', crate.name);
-									groupController.destroyUnit( serverName, crate.name );
-								})
-								.catch(function (err) {
-									console.log('line 56: ', err);
-								})
-							;
-						}
-					});
-				})
-				.catch(function (err) {
-					console.log('err line63: ', err);
-				})
-			;
-		}
 
 		dbMapServiceController.baseActions('read', serverName, {mainBase: true, $or: [{side: 1}, {side: 2}]})
 			.then(function (bases) {
--- controllers/timedEvents/thirtySecs.js
+++ controllers/timedEvents/thirtySecs.js
@@ -1,7 +1,12 @@
 const _ = require('lodash');
 const dbMapServiceController = require('../db/dbMapService');
 const jtacController = require('../action/jtac');
+const menuUpdateController = require('../menu/menuUpdate');
+const groupController = require('../spawn/group');
 const troopLocalizerController = require('../action/troopLocalizer');
+
+var AIMaxIdleTime = (5 * 60 * 1000); // 5 mins
+var maxCrateLife = (3 * 60 * 60 * 1000); // 3 hrs
 
 _.set(exports, 'processThirtySecActions', function (serverName, fullySynced) {
 	if (fullySynced) {
@@ -16,5 +21,56 @@ _.set(exports, 'processThirtySecActions', function (serverName, fullySynced) {
 
 		jtacController.aliveJtac30SecCheck(serverName);
 		// troopLocalizerController.checkTroopProx(serverName);
+
+		//cleanupAI AIMaxIdleTime
+		dbMapServiceController.unitActions('read', serverName, {isAI: true, dead:false})
+			.then(function (AICleanup) {
+				_.forEach(AICleanup, function (AIUnit) {
+					if (_.isEmpty(AIUnit.playername) && new Date(_.get(AIUnit, 'updatedAt', 0)).getTime() + AIMaxIdleTime < new Date().getTime()) {
+						groupController.destroyUnit( serverName, AIUnit.name );
+					}
+				});
+			})
+			.catch(function (err) {
+				console.log('err line20: ', err);
+			})
+		;
+
+		//clean crates older than 90mins
+		if (menuUpdateController.virtualCrates) {
+			dbMapServiceController.unitActions('read', serverName, {isCrate: true, dead:false})
+				.then(function (crateCleanup) {
+					_.forEach(crateCleanup, function (crate) {
+						if (new Date(_.get(crate, 'createdAt', 0)).getTime() + maxCrateLife < new Date().getTime()) {
+							groupController.destroyUnit( serverName, crate.name );
+						}
+					});
+				})
+				.catch(function (err) {
+					console.log('err line42: ', err);
+				})
+			;
+		} else {
+			dbMapServiceController.staticCrateActions('readStd', serverName, {})
+				.then(function (crateCleanup) {
+					_.forEach(crateCleanup, function (crate) {
+						if (new Date(_.get(crate, 'createdAt', 0)).getTime() + maxCrateLife < new Date().getTime()) {
+							dbMapServiceController.staticCrateActions('delete', serverName, {_id: crate._id})
+								.then(function () {
+									console.log('cleanup crate: ', crate.name);
+									groupController.destroyUnit( serverName, crate.name );
+								})
+								.catch(function (err) {
+									console.log('line 56: ', err);
+								})
+							;
+						}
+					});
+				})
+				.catch(function (err) {
+					console.log('err line63: ', err);
+				})
+			;
+		}
 	}
 });
