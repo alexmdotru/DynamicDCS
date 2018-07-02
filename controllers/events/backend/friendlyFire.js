a996b791c32daa8689b00322a8a0c3ec5628871d
Andrew Finegan <andrew.finegan@gmail.com>
2018-07-01 20:40:34 -0400

    kill each other

--- controllers/events/backend/friendlyFire.js
+++ controllers/events/backend/friendlyFire.js
@@ -3,6 +3,7 @@ const constants = require('../../constants');
 const dbMapServiceController = require('../../db/dbMapService');
 const DCSLuaCommands = require('../../player/DCSLuaCommands');
 const playersEvent = require('../../events/backend/players');
+const groupController = require('../spawn/group');
 const userLivesController = require('../../action/userLives');
 
 _.set(exports, 'processFriendlyFire', function (serverName, sessionName, eventObj) {
