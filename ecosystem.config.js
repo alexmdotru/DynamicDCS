799d3f5877a4be6ad43f37aed98ba472b9505c4a
Andrew Finegan <andrew.finegan@gmail.com>
2018-10-03 22:15:58 -0400

    pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   apps : [
       {
+          name: 'DDCS_Webapp',
+          script: 'index.js',
+          // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+          instances: 1,
+          autorestart: true,
+          watch: false,
+          max_memory_restart: '1G',
+          env: {
+              NODE_ENV: 'development'
+          },
+          env_production: {
+              NODE_ENV: 'production'
+          }
+      },
+      {
           name: 'DDCS_Standard_Front',
           script: 'servers/DDCSStandard/DDCSStandard-Front.js',
           // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
