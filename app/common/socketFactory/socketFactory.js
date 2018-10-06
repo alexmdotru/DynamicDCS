f9173835c30bcc406a4e90d028b238bb94f7baab
Andrew Finegan <andrew.finegan@gmail.com>
2018-10-05 23:14:20 -0400

    cleanup

--- app/common/socketFactory/socketFactory.js
+++ app/common/socketFactory/socketFactory.js
@@ -2,8 +2,6 @@
 	'use strict';
 
 	function SocketFactoryController (socketFactory) {
-		var jart = this;
-		console.log('sock: ', jart);
 		var myIoSocket = io.connect('/', {
 			query: 'token=Bearer '+localStorage.getItem('access_token')+'&authId='+localStorage.getItem('sub')
 		});
