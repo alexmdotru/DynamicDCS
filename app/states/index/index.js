2f94ecf4ccc82f3a06404ba8fd782aed4ae62791
Andrew Finegan <andrew.finegan@gmail.com>
2018-10-08 22:42:49 -0400

    cleanup

--- app/states/index/index.js
+++ app/states/index/index.js
@@ -1,29 +1,11 @@
 (function (angular) {
 	'use strict';
 
-	function indexController (srvService, mySocket, eventService) {
-		var indxCtrl = this;
-		_.set(indxCtrl, 'srvService', srvService);
-		_.set(indxCtrl, 'eventService', eventService);
-
-		mySocket.emit('room', {
-			server: 'index'
-		});
-
-		mySocket.on('srvUpd', function (data) {
-			console.log('IData: ', data);
-		});
-	}
-	indexController.$inject = ['srvService', 'mySocket', 'eventService'];
-
 	function configFunction($stateProvider) {
 		$stateProvider
 			.state('index', {
-				controller: 'indexController',
-				controllerAs: 'indxCtrl',
 				templateUrl: '/apps/ddcs/states/index/index.tpl.html',
-				url: '/',
-				bindToController: true
+				url: '/'
 			})
 		;
 	}
@@ -35,11 +17,9 @@
 
 	angular
 		.module('state.index', [
-			'ui.router',
-			'highcharts-ng'
+			'ui.router'
 		])
 		.config(['$stateProvider', '$urlRouterProvider', configFunction])
 		.run(authHandler)
-		.controller('indexController', indexController)
 	;
 }(angular));
