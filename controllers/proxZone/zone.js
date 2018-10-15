c4a9c172411b56e8be4570e8b085e102d0424632
Andrew Finegan <andrew.finegan@gmail.com>
2018-10-15 19:49:30 -0400

    more cleanup

--- controllers/proxZone/zone.js
+++ controllers/proxZone/zone.js
@@ -1,6 +1,6 @@
 const	_ = require('lodash');
 const constants = require('../constants');
-const dbMapServiceController = require('../db/dbMapService');z
+const dbMapServiceController = require('../db/dbMapService');
 
 /**
  * Calculate a new coordinate based on start, distance and bearing
