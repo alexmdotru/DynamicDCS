74b1f528eda01f2450a275f37a85e9bbba684d9c
Andrew Finegan <andrew.finegan@gmail.com>
2018-12-13 18:59:47 -0500

    pretty much ready to go

--- controllers/events/frontend/S_EVENT_LAND.js
+++ controllers/events/frontend/S_EVENT_LAND.js
@@ -27,7 +27,6 @@ _.set(exports, 'processEventLand', function (serverName, sessionName, eventObj) 
 					var curUnitDict = _.find(constants.unitDictionary, {_id: curIUnit.type});
 					var curLifePointVal = (curUnitDict) ? curUnitDict.lifeCost : 1;
 					if (curIUnit) {
-						console.log('land: ', _.get(curIUnit, 'playername'), baseLand);
 						//landed logistic planes/helis spawn new group for area
 						var curUnitName = _.get(curIUnit, 'name');
 						if (_.includes(curUnitName, 'LOGISTICS|')) {
@@ -56,6 +55,7 @@ _.set(exports, 'processEventLand', function (serverName, sessionName, eventObj) 
 						}
 
 						iPlayer = _.find(playerArray, {name: _.get(curIUnit, 'playername')});
+						console.log('land: ', _.get(curIUnit, 'playername'), baseLand, iPlayer);
 						if(iPlayer) {
 							if (baseLand) {
 								masterDBController.baseActions('read', serverName, {_id: baseLand})
