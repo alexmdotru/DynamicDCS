f703f582656672d94546261b1870ca024699edad
Andrew Finegan <andrew.finegan@gmail.com>
2018-12-13 22:20:40 -0500

    pretty much ready to go

--- controllers/events/backend/friendlyFire.js
+++ controllers/events/backend/friendlyFire.js
@@ -49,7 +49,7 @@ _.set(exports, 'processFriendlyFire', function (serverName, sessionName, eventOb
 					masterDBController.srvPlayerActions('read', serverName, {_id: tPlayer.ucid})
 						.then(function (tPlayers) {
 							var curTPlayer = _.first(tPlayers);
-							console.log('SAT: ', _.get(curIPlayer, 'safeLifeActionTime', 0) < new Date().getTime());
+							console.log('SAT: ', _.get(curIPlayer, 'safeLifeActionTime', 0) < new Date().getTime(), _.get(curIPlayer, 'safeLifeActionTime', 0), new Date().getTime());
 							if(_.get(curIPlayer, 'safeLifeActionTime', 0) < new Date().getTime()) {
 								masterDBController.unitActions('read', serverName, {unitId: iPlayer.slot})
 									.then(function (iunit) {
