85893fd614c8e59e17de9851e1ad13723d6c8372
Andrew Finegan <andrew.finegan@gmail.com>
2019-01-16 23:11:09 -0500

    fixing EWR

--- controllers/spawn/group.js
+++ controllers/spawn/group.js
@@ -872,8 +872,8 @@ _.set(exports, 'grndUnitGroup', function ( groupObj, task, routes ) {
 	if (routes) {
 		curRoute = routes;
 	} else if (groupObj.type === '1L13 EWR' || groupObj.type === '55G6 EWR' ) {
-		console.log('turningOnRouteEWRInstructions: ', groupObj);
 		_.set(groupObj, 'country', 'UKRAINE');
+		console.log('turningOnRouteEWRInstructions: ', groupObj);
 		curRoute = exports.turnOnEWRAuto();
 	} else {
 		curRoute = exports.turnOffDisperseUnderFire();
