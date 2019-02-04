f93d177a65975895899f90c577107894fc25b3cb
Andrew Finegan <andrew.finegan@gmail.com>
2019-02-04 11:18:38 -0500

    disable voice comms

--- controllers/discordBot/commsUserProcessing.js
+++ controllers/discordBot/commsUserProcessing.js
@@ -120,11 +120,11 @@ _.set(dBot, 'kickForNoComms', function (serverName, playerArray, isDiscordAllowe
                             }
 
                             if (!((curPlayerCommObj.isInSRS && serverName === _.get(curPlayerCommObj, 'SRSData.SRSServer')) || (curPlayerCommObj.isInDiscord && isDiscordAllowed))) {
-                                console.log(curPlayerName + 'NOT in voice comms');
+                                // console.log(curPlayerName + 'NOT in voice comms');
                                 // dBot.processKick(serverName, curPlayer,  curPlayerCommObj, isDiscordAllowed, curPlayerUnit);
                             }
                         } else {
-                            console.log( curPlayer.name + ' NOT a member of DDCS community');
+                            // console.log( curPlayer.name + ' NOT a member of DDCS community');
                             // dBot.processKick(serverName, curPlayer, curPlayerCommObj, isDiscordAllowed, curPlayerUnit);
                         }
                     })
