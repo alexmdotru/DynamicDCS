a43d3d38b78c544a9a470134cfec123a9deb3fab
Andrew Finegan <andrew.finegan@gmail.com>
2019-02-04 11:19:20 -0500

    disable voice comms

--- controllers/action/jtac.js
+++ controllers/action/jtac.js
@@ -3,7 +3,7 @@ const constants = require('../constants');
 const masterDBController = require('../db/masterDB');
 const proximityController = require('../proxZone/proximity');
 
-var jtacDistance = 3;
+var jtacDistance = 5;
 var curLaserCode = 1688;
 var redLaserCode = 1686;
 var blueLaserCode = 1687;
