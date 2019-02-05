0899c9bdfabd9071bc342f5139511ce5b4d022a1
Andrew Finegan <andrew.finegan@gmail.com>
2019-02-05 12:31:27 -0500

    Clean Up

--- auth0-variables.js
+++
@@ -1,5 +0,0 @@
-var AUTH0_CLIENT_ID='3TPd6XVVFb2g3BhjppKdCz5X8q4Mraek';
-var AUTH0_DOMAIN='afinegan.auth0.com';
-var AUTH0_CALLBACK_URL='http://dynamicdcs.com:80/';
-var AUTH0_AUDIENCE='https://afinegan.auth0.com/api/v2/';
-var REQUESTED_SCOPES='openid profile';
