22599c8988606ce213a5a08bc0ff2a41045e42ff
Andrew Finegan <andrew.finegan@gmail.com>
2019-02-10 02:26:07 -0500

    auto repair for sams every 10 mins entering service

--- controllers/timedEvents/tenMinutes.js
+++ controllers/timedEvents/tenMinutes.js
@@ -1,6 +1,10 @@
 const _ = require('lodash');
 const userLivesController = require('../action/userLives');
+const groupController = require('../menu/repair');
 
 _.set(exports, 'processTenMinuteActions', function (serverName, fullySynced) {
-	if (fullySynced) { userLivesController.updateServerLifePoints(serverName) }
+	if (fullySynced) {
+		userLivesController.updateServerLifePoints(serverName)
+		groupController.repairBaseSAMRadars(serverName);
+	}
 });
