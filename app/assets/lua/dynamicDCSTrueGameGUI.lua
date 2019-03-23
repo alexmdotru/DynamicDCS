b51379b3df9f941d68260b89795b1ac4a6ce0624
Andrew Finegan <andrew.finegan@gmail.com>
2019-03-23 19:33:45 -0400

    working slot lock with permissions

--- app/assets/lua/dynamicDCSTrueGameGUI.lua
+++ app/assets/lua/dynamicDCSTrueGameGUI.lua
@@ -21,6 +21,10 @@ local updateQue = {["que"] = {} }
 local PORT = 3002
 local DATA_TIMEOUT_SEC = 1
 
+dynDCS.kickTimeInterval = 1
+dynDCS.kickReset = true
+dynDCS.kickTimePrev = 0
+
 totalPlayers = 80
 
 isLoadLock = false
@@ -71,7 +75,6 @@ function string:split( inSplitPattern, outResults )
 end
 
 local function getDataMessage()
-	--chunk send back updateQue.que
 	local chkSize = 500
 	local payload = {}
 	payload.que = {}
@@ -79,7 +82,6 @@ local function getDataMessage()
 		table.insert(payload.que, updateQue.que[i])
 		table.remove(updateQue.que, i)
 	end
-	--log(JSON:encode(playerSync()))
 	dynDCS.curPlayers = dynDCS.playerSync()
 	table.insert(payload.que, dynDCS.curPlayers)
 	table.insert(payload.que, {["action"] = 'mission', ["data"] = DCS.getMissionFilename()})
@@ -88,12 +90,7 @@ end
 
 local function runRequest(request)
 	if request.action ~= nil then
-		--if request.action == "INIT" then
-		--	log('RUNNING REQUEST INIT')
-		--	cacheDB = {}
-		--end
 		if request.action == "CMD" and request.cmd ~= nil and request.reqID ~= nil then
-			--net.log('RUNNING CMD')
 			pcallCommand(request.cmd, request.reqID)
 		end
 	end
@@ -128,7 +125,6 @@ end
 
 local client
 local function step()
-
 	if not client then
 		tcp:settimeout(0.001)
 		client = tcp:accept()
@@ -139,7 +135,6 @@ local function step()
 			clearVar()
 		end
 	end
-
 	if client then
 		local line, err = client:receive()
 		if line ~= nil then
@@ -176,39 +171,23 @@ local function step()
 		end
 	end
 end
-
 dynDCS.playerSync = function ()
 	local refreshPlayer = {}
 	local playerTable = {}
 	playerTable.action = 'players'
 	playerTable.data = {}
-
-	--buildPlayerTable
 	local curPlayers = net.get_player_list()
 	for key,value in pairs(curPlayers) do
 		playerTable.data[value] = net.get_player_info(value)
 		refreshPlayer[value] = 1
-		--local _playerId = playerTable.data[value].id
-		--local _slotId = playerTable.data[value].slot
-		--local _side = playerTable.data[value].side
-
-		--if  (_side ~=0 and  slotID ~='' and _slotId ~= nil)  then
-		--	if not dynDCS.shouldAllowSlot(_playerId, _slotId) then
-		--		dynDCS.rejectPlayer(_playerId)
-		--	end
-		--end
 	end
-
 	for k, v in pairs( playerTable.data ) do
 		if refreshPlayer[k] == nil then
 			playerTable.data[k] = nil
 		end
 	end
-
-	--net.log('playertable: '..JSON:encode(playerTable))
 	return playerTable
 end
-
 local _lastSent = 0
 dynDCS.onSimulationFrame = function()
 	local _now = DCS.getRealTime()
@@ -218,25 +197,172 @@ dynDCS.onSimulationFrame = function()
 		if not success then
 			log("Error: " .. error)
 		end
-		if updateQue.que ~= nil then
-			--log('LogBuffer: '..table.getn(updateQue.que))
-		end
 	end
 end
 
---dynDCS.onChatMessage = function(message,playerID)
---	if( message ~= nil ) then
---		--log(message)
---		local curUpdate = {
---			action = 'MESG',
---			data = {
---				message = message,
---				playerID = playerID
---			}
---		}
---		table.insert(updateQue.que, curUpdate)
---	end
---end
+function pcallCommand(s, respID)
+	local success, resp =  pcall(commandExecute, s)
+	if not success then
+		log("Error: " .. resp)
+	end
+end
+
+function commandExecute(s)
+	return loadstring("return " ..s)()
+end
+
+function dynDCS.getFlagValue(_flag)
+	local _status,_error  = net.dostring_in('server', " return trigger.misc.getUserFlag(\"".._flag.."\"); ")
+	if not _status and _error then
+		return 0
+	else
+		return tonumber(_status)
+	end
+end
+
+function dynDCS.getUnitId(_slotID)
+	local _unitId = tostring(_slotID)
+	if string.find(tostring(_unitId),"_",1,true) then
+		_unitId = string.sub(_unitId,1,string.find(_unitId,"_",1,true))
+	end
+	return tonumber(_unitId)
+end
+
+function dynDCS.shouldAllowSlot(_playerID, slotID, side)
+	isLoadLock = false
+	isRedPlayerMax = false
+	isBluePlayerMax = false
+	isRedLocked = false
+	isBlueLocked = false
+	isGamemasterLock = false
+
+	local curSides = {
+		["red"] = 0,
+		["blue"] = 0
+	}
+
+	local curPlayers = net.get_player_list()
+	for key,value in pairs(curPlayers) do
+		local playerInfo = net.get_player_info(value);
+		if playerInfo.id ~= _playerID then
+			if playerInfo.side == 1 then
+				curSides.red = curSides.red + 1
+			end
+			if playerInfo.side == 2 then
+				curSides.blue = curSides.blue + 1
+			end
+		end
+	end
+	if side == 1 then
+		curSides.red = curSides.red + 1
+	end
+	if side == 2 then
+		curSides.blue = curSides.blue + 1
+	end
+
+	local _isOpenSlot = dynDCS.getFlagValue('isOpenSlot')
+	if _isOpenSlot ~= nil then
+		_isOpenSlot = tonumber(_isOpenSlot)
+	else
+		_isOpenSlot = 0
+	end
+	if _isOpenSlot == 0 then
+		isLoadLock = true
+		return false
+	end
+
+	local curUcid = net.get_player_info(_playerID, 'ucid')
+	if string.find(tostring(slotID),"instructor",1,true) then
+		if curUcid == 'd124b99273260cf876203cb63e3d7791' then
+			return true
+		end
+		isGamemasterLock = true
+		return false
+	end
+	local _ucidFlagRed = dynDCS.getFlagValue(curUcid..'_1')
+	local _ucidFlagBlue = dynDCS.getFlagValue(curUcid..'_2')
+	local _unitId = dynDCS.getUnitId(slotID)
+
+	if _unitId == nil then
+		local curColor = slotID:split('_')[3]
+		if curColor == 'red' and curSides.red > totalPerSide then
+			isRedPlayerMax = true
+			return false
+		end
+		if curColor == 'blue' and curSides.blue > totalPerSide  then
+			isBluePlayerMax = true
+			return false
+		end
+		if _ucidFlagRed == 1 and curColor == 'blue' then
+			isRedLocked = true
+			return false
+		end
+		if _ucidFlagBlue == 1 and curColor == 'red' then
+			isBlueLocked = true
+			return false
+		end
+		return true
+	end
+
+	local curSide = coalitionLookup[DCS.getUnitProperty(slotID, DCS.UNIT_COALITION)]
+	local curBaseName = DCS.getUnitProperty(slotID, DCS.UNIT_NAME):split(' #')[1]:split("_Extension")[1]
+	local _baseFlag = dynDCS.getFlagValue(curBaseName)
+	if _baseFlag == curSide then
+		if curSides.red > totalPerSide and curSide == 1 then
+			isRedPlayerMax = true
+			return false
+		end
+		if curSides.blue > totalPerSide and curSide == 2 then
+			isBluePlayerMax = true
+			return false
+		end
+		if _ucidFlagRed == 1 and _baseFlag == 2 then
+			isRedLocked = true
+			return false
+		end
+		if _ucidFlagBlue == 1 and _baseFlag == 1 then
+			isBlueLocked = true
+			return false
+		end
+		return true
+	end
+	if curBaseName == 'Carrier1' and _ucidFlagBlue ~= 1 then
+		return true
+	end
+	if curBaseName == 'Carrier2' and _ucidFlagRed ~= 1 then
+		return true
+	end
+	return false
+end
+
+dynDCS.rejectPlayer = function(playerID)
+	net.force_player_slot(playerID, 0, '')
+	local _playerName = net.get_player_info(playerID, 'name')
+	if _playerName == nil then
+		_playerName = ""
+	end
+	if _playerName ~= nil then
+		local _chatMessage
+		if(isLoadLock) then
+			_chatMessage = "***Slot DISABLED, Server Is Syncing Units***"
+		elseif (isGamemasterLock) then
+			_chatMessage = "***Slot DISABLED, Slot is only for Game Masters***"
+		elseif (isRedPlayerMax) then
+			_chatMessage = "***Side DISABLED, Maximum Amount Of Red Players Reached("..totalPerSide..")***"
+		elseif (isBluePlayerMax) then
+			_chatMessage = "***Side DISABLED, Maximum Amount Of Blue Players Reached("..totalPerSide..")***"
+		elseif (isRedLocked) then
+			_chatMessage = "***Slot DISABLED, You Are Locked To Red Side This Session***"
+		elseif (isBlueLocked) then
+			_chatMessage = "***Slot DISABLED, You Are Locked To Blue Side This Session***"
+		else
+			_chatMessage = "***Slot DISABLED, Capture This Airport***"
+		end
+		net.send_chat_to(_chatMessage, playerID)
+		return false
+	end
+	return true
+end
 
 dynDCS.onGameEvent = function(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7, arg8, arg9)
 	local curUpdate = {}
@@ -260,44 +386,6 @@ dynDCS.onGameEvent = function(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7, arg8
 		}
 		table.insert(updateQue.que, curUpdate)
 	end
-	--if( eventName == "mission_end" ) then
-	--"mission_end", winner, msg
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-	--if( eventName == "kill" ) then
-	--	--"kill", killerPlayerID, killerUnitType, killerSide, victimPlayerID, victimUnitType, victimSide, weaponName
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
 	if( eventName == "self_kill" ) then
 		--"self_kill", playerID
 		curUpdate = {
@@ -374,316 +462,25 @@ dynDCS.onGameEvent = function(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7, arg8
 		}
 		table.insert(updateQue.que, curUpdate)
 	end
-	--if( eventName == "crash" ) then
-	--	--"crash", playerID, unit_missionID
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-	--if( eventName == "eject" ) then
-	--	--"eject", playerID, unit_missionID
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-	--if( eventName == "takeoff" ) then
-	--	--"takeoff", playerID, unit_missionID, airdromeName
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-	--if( eventName == "landing" ) then
-	--	--"landing", playerID, unit_missionID, airdromeName
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-	--if( eventName == "pilot_death" ) then
-	--	--"pilot_death", playerID, unit_missionID
-	--	curUpdate = {
-	--		action = eventName,
-	--		data = {
-	--			name = eventName,
-	--			arg1 = arg1,
-	--			arg2 = arg2,
-	--			arg3 = arg3,
-	--			arg4 = arg4,
-	--			arg5 = arg5,
-	--			arg6 = arg6,
-	--			arg7 = arg7,
-	--			arg8 = arg8,
-	--			arg9 = arg9
-	--		}
-	--	}
-	--	table.insert(updateQue.que, curUpdate)
-	--end
-end
-
---Protected call to command execute
-function pcallCommand(s, respID)
-	local success, resp =  pcall(commandExecute, s)
-	if success then
-		if resp then
-			--local curUpdate
-			--curUpdate = {
-			--	action = 'CMDRESPONSE',
-			--	data = {
-			--		respID = respID,
-			--		cmd = s,
-			--		response = resp
-			--	}
-			--}
-			--table.insert(updateQue.que, curUpdate)
-		end
-	else
-		log("Error: " .. resp)
+	if( eventName == "change_slot" ) then
+		net.log('change slot: '..arg1..' '..arg2)
 	end
 end
 
-function commandExecute(s)
-	return loadstring("return " ..s)()
-end
-
-function dynDCS.getFlagValue(_flag)
-	local _status,_error  = net.dostring_in('server', " return trigger.misc.getUserFlag(\"".._flag.."\"); ")
-	if not _status and _error then
-		--net.log("error getting flag: ".._error)
-		return 0
-	else
-		--net.log("flag value ".._flag.." value: ".._status)
-		--disabled
-		return tonumber(_status)
-	end
-end
-
-function dynDCS.getUnitId(_slotID)
-	local _unitId = tostring(_slotID)
-	if string.find(tostring(_unitId),"_",1,true) then
-		_unitId = string.sub(_unitId,1,string.find(_unitId,"_",1,true))
-		--net.log("Unit ID Substr ".._unitId)
-	end
-	return tonumber(_unitId)
-end
-
-function dynDCS.shouldAllowSlot(_playerID, _slotID, side)
-	isLoadLock = false
-	isRedPlayerMax = false
-	isBluePlayerMax = false
-	isRedLocked = false
-	isBlueLocked = false
-	isGamemasterLock = false
-
-	local curSides = {
-		["red"] = 0,
-		["blue"] = 0
-	}
-
-	local curPlayers = net.get_player_list()
-	for key,value in pairs(curPlayers) do
-		local playerInfo = net.get_player_info(value);
-		if playerInfo.id ~= _playerID then
-			if playerInfo.side == 1 then
-				curSides.red = curSides.red + 1
-			end
-			if playerInfo.side == 2 then
-				curSides.blue = curSides.blue + 1
+dynDCS.onPlayerChangeSlot = function(playerID)
+	if  DCS.isServer() and DCS.isMultiplayer() then
+		local slotID = net.get_player_info(playerID, 'slot')
+		local side = net.get_player_info(playerID, 'side')
+		if side ~=0 and  slotID ~='' and slotID ~= nil then
+			local _allow = dynDCS.shouldAllowSlot(playerID, slotID, side)
+			if not _allow then
+				dynDCS.rejectPlayer(playerID)
+				return
 			end
 		end
-	end
-	if side == 1 then
-		curSides.red = curSides.red + 1
-	end
-	if side == 2 then
-		curSides.blue = curSides.blue + 1
-	end
-
-	local curSlot = net.get_player_info(_playerID, 'slot')
-	local _isOpenSlot = dynDCS.getFlagValue('isOpenSlot')
-	--net.log('io'.._playerID..' '.._slotID..' '.._isOpenSlot)
-	if _isOpenSlot ~= nil then
-		_isOpenSlot = tonumber(_isOpenSlot)
-	else
-		_isOpenSlot = 0
-	end
-	--net.log('lockflag '.. _isOpenSlot)
-	if _isOpenSlot == 0 then
-		isLoadLock = true
-		return false
-	end
-
-	local curUcid = net.get_player_info(_playerID, 'ucid')
-	if string.find(tostring(_slotID),"instructor",1,true) then
-		--net.log('slotid: '.._slotID..' ucid: '..curUcid)
-		if curUcid == 'd124b99273260cf876203cb63e3d7791' then
-			return true
-		end
-		isGamemasterLock = true
-		return false
-	end
-	local _ucidFlagRed = dynDCS.getFlagValue(curUcid..'_1')
-	local _ucidFlagBlue = dynDCS.getFlagValue(curUcid..'_2')
-	local _unitId = dynDCS.getUnitId(_slotID)
-
-	if _unitId == nil then
-		local curColor = _slotID:split('_')[3]
-		if curColor == 'red' and curSides.red > totalPerSide then
-			--net.log('max red players reached')
-			isRedPlayerMax = true
-			return false
-		end
-		if curColor == 'blue' and curSides.blue > totalPerSide  then
-			--net.log('max blue players reached')
-			isBluePlayerMax = true
-			return false
-		end
-		if _ucidFlagRed == 1 and curColor == 'blue' then
-			isRedLocked = true
-			return false
-		end
-		if _ucidFlagBlue == 1 and curColor == 'red' then
-			isBlueLocked = true
-			return false
-		end
-		return true
-	end
-
-	local curSide = coalitionLookup[DCS.getUnitProperty(_slotID, DCS.UNIT_COALITION)]
-	-- local curType = DCS.getUnitProperty(_slotID, DCS.UNIT_TYPE)
-	local curBaseName = DCS.getUnitProperty(_slotID, DCS.UNIT_NAME):split(' #')[1]:split("_Extension")[1]
-	local _baseFlag = dynDCS.getFlagValue(curBaseName)
-	--net.log(curBaseName.."_".._unitId..' flag:'.._baseFlag..' uSide:'..curSide..' ucidFlag: '.._ucidFlag..' ucid:'..curUcid)
-	--net.log('CBN: '..curBaseName)
-	--net.log('totalRed:'..curSides.red..' totalBlue:'..curSides.blue..' slotId:'..curSlot)
-	if _baseFlag == curSide then
-		--net.log('STUFFF '..capLives[curType]..' - '..curType..' ucid: '.._ucidFlag)
-		if curSides.red > totalPerSide and curSide == 1 then
-			--net.log('max red players reached')
-			isRedPlayerMax = true
-			return false
-		end
-		if curSides.blue > totalPerSide and curSide == 2 then
-			--net.log('max blue players reached')
-			isBluePlayerMax = true
-			return false
-		end
-		if _ucidFlagRed == 1 and _baseFlag == 2 then
-			--net.log('User red locked')
-			isRedLocked = true
-			return false
-		end
-		if _ucidFlagBlue == 1 and _baseFlag == 1 then
-			--net.log('User blue locked')
-			isBlueLocked = true
-			return false
-		end
-		--net.log('Base Slot Open')
-		return true
-	end
-	if curBaseName == 'Carrier1' and _ucidFlagBlue ~= 1 then
-		return true
-	end
-	if curBaseName == 'Carrier2' and _ucidFlagRed ~= 1 then
-		return true
-	end
-	return false
-end
-
-dynDCS.rejectPlayer = function(playerID)
-	net.force_player_slot(playerID, 0, '')
-	local _playerName = net.get_player_info(playerID, 'name')
-	if _playerName == nil then
-		_playerName = ""
-	end
-	if _playerName ~= nil then
-		--Disable chat message to user
-		local _chatMessage
-		if(isLoadLock) then
-			_chatMessage = "***Slot DISABLED, Server Is Syncing Units***"
-		elseif (isGamemasterLock) then
-			_chatMessage = "***Slot DISABLED, Slot is only for Game Masters***"
-		elseif (isRedPlayerMax) then
-			_chatMessage = "***Side DISABLED, Maximum Amount Of Red Players Reached("..totalPerSide..")***"
-		elseif (isBluePlayerMax) then
-			_chatMessage = "***Side DISABLED, Maximum Amount Of Blue Players Reached("..totalPerSide..")***"
-		elseif (isRedLocked) then
-			_chatMessage = "***Slot DISABLED, You Are Locked To Red Side This Session***"
-		elseif (isBlueLocked) then
-			_chatMessage = "***Slot DISABLED, You Are Locked To Blue Side This Session***"
-		else
-			_chatMessage = "***Slot DISABLED, Capture This Airport***"
-		end
-		net.send_chat_to(_chatMessage, playerID)
-		return false
 	end
 	return true
 end
 
-dynDCS.onPlayerTryChangeSlot = function(playerID, side, slotID)
-	--net.log("SLOT - allowed -  playerid: "..playerID.." side:"..side.." slot: "..slotID)
-	if  DCS.isServer() and DCS.isMultiplayer() then
-		if  (side ~=0 and  slotID ~='' and slotID ~= nil)  then
-			--net.log('netslot '..slotID)
-			local _allow = dynDCS.shouldAllowSlot(playerID,slotID, side)
-			if not _allow then
-				return dynDCS.rejectPlayer(playerID)
-			end
-		end
-		return true
-	end
-end
-
 DCS.setUserCallbacks(dynDCS)
-
-net.log("Loaded - DynDCSGameGUI Server started")
+net.log("Loaded - DDCSGameGUI Server started")
